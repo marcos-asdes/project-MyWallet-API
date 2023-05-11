2ad686d2a3c171a3103aa06bf6f3b2a3293276af
marcos.asdes <marco.engc@poli.ufrj.br>
2023-05-11 13:47:17 -0300

    refactor: transactions repository were created

--- src/controllers/transactionsController.ts
+++ src/controllers/transactionsController.ts
@@ -1,42 +1,24 @@
-import dayjs from 'dayjs';
-
-import database from '../config.js';
-
-import transactionsSchema from '../schemas/transactionsSchema.js';
 import { Request, Response } from 'express';
+import dayjs from 'dayjs';
+import transactionsSchema from '../schemas/transactionsSchema.js';
 
-export async function getTransactions(_req: Request, res: Response) {
-  const { user } = res.locals;
-  try {
-    const transactions = await database
-      .collection('transactions')
-      .find({ userId: user._id })
-      .toArray();
-    res.send(transactions);
-  } catch (error) {
-    console.log('Error getting all financial transactions', error);
-    return res.sendStatus(500);
-  }
+async function getTransactionsController(
+  _req: Request,
+  res: Response
+): Promise<Response> {
+  // tem q ter um middleware de autenticação
+  // pega as transações no banco de dados baseados no id do usuário
+  return res.sendStatus(200);
+  //return res.send(transactions);
 }
 
-export async function addTransactions(req: Request, res: Response) {
-  const { error } = transactionsSchema.validate(req.body);
-  if (error)
-    return res.status(422).send(error.details.map(detail => detail.message)); // unprocessable entity
-
-  const { user } = res.locals;
-  try {
-    const { type, description, value } = req.body;
-    await database.collection('transactions').insertOne({
-      type,
-      value,
-      description,
-      date: dayjs().format('DD/MM'),
-      userId: user._id
-    });
-    res.sendStatus(201);
-  } catch (error) {
-    console.log('Error adding new transaction', error);
-    return res.sendStatus(500);
-  }
+async function addTransactionsController(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  // executar a validação de schema no middleware, erro 422
+  // adicionar transação no bd (verificar regras de saldo?)
+  return res.sendStatus(201);
 }
+
+export { getTransactionsController, addTransactionsController };
---
+++ src/repositories/transactionsRepository.ts
@@ -0,0 +1,35 @@
+import { Document, WithId } from 'mongodb';
+import db from '../config.js';
+import { ErrorLog } from '../events/errorHandler.js';
+import logHandler from '../events/logHandler.js';
+import dayjs from 'dayjs';
+
+async function getTransactionsFromDatabase(
+  userId: string
+): Promise<WithId<Document>[]> {
+  if (!db) throw new ErrorLog(500, 'Database connection not established');
+  const transactions = await db
+    .collection('transactions')
+    .find({ userId: userId })
+    .toArray();
+  logHandler('Repository', 'Repository accessed successfully');
+  return transactions;
+}
+
+async function addTransactionInDatabase(
+  type: string,
+  value: number,
+  description: string,
+  userId: string
+): Promise<void> {
+  if (!db) throw new ErrorLog(500, 'Database connection not established');
+  await db.collection('transactions').insertOne({
+    type,
+    value,
+    description,
+    date: dayjs().format('DD/MM'),
+    userId: userId
+  });
+  logHandler('Repository', 'Repository accessed successfully');
+}
+export { getTransactionsFromDatabase, addTransactionInDatabase };
