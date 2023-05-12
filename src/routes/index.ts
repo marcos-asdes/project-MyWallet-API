98f6beffa677137e8b7ece7d07b51f538db30c34
marcos.asdes <marco.engc@poli.ufrj.br>
2023-05-12 19:14:17 -0300

    doc: swagger added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
-
+import swaggerUi from 'swagger-ui-express';
 import authRouter from './authRouter.js';
 import transactionsRouter from './transactionsRouter.js';
+import swaggerDocs from '../../swagger.json';
 import { routeLog } from '../events/routeLog.js';
 
 const router = express.Router();
 
 const api = '/api';
+const doc = api + '/doc';
 
+router.use(doc, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 router.use(api, authRouter);
 router.use(api, transactionsRouter);
 
