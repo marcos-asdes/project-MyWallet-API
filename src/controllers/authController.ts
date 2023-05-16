ca057c20f401f2dd5ebf0e18c06d30ee153b0873
marcos.asdes <marco.engc@poli.ufrj.br>
2023-05-15 23:14:49 -0300

    refactor: signInController has been refactored

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,38 +1,35 @@
 import { Request, Response } from 'express';
+import { Document, ObjectId, WithId } from 'mongodb';
 import logHandler from '../events/logHandler.js';
 import { authRepository } from '../repositories/authRepository.js';
 import * as service from '../services/authService.js';
 
-async function signUpController(
-  _req: Request,
-  res: Response
-): Promise<Response> {
+async function signUpController(req: Request, res: Response): Promise<Response> {
   const { name, email, password } = res.locals.body;
-  await service.checkIfUserIsAlreadyRegistered(email);
-  const encryptedPassword = await service.encryptPassword(password);
+  const route: string = req.url;
+  await service.checkIfUserIsRegistered(email, route);
+  const encryptedPassword: string = await service.encryptPassword(password);
   await authRepository.registerUserInDatabase(name, email, encryptedPassword);
-  const data = `User ${email} has been registered successfully.`;
+  const data: string = `User ${email} has been registered successfully.`;
   logHandler('Controller', 'User signed up');
   return res.status(201).send(data);
 }
 
-async function signInController(
-  req: Request,
-  res: Response
-): Promise<Response> {
-  // verifica se o usuário existe
-  // verifica se o login está correto (email e senha batem)
-  // verifica se o usuario já esta logado ???
-  // cria token de autenticação do usuário
-  // cria sessão do usuario (dá pra acrescentar mais lógicas aqui)
-  return res.sendStatus(200);
-  //return res.send({ token, name: user.name });
+async function signInController(req: Request, res: Response): Promise<Response> {
+  const { email, password } = res.locals.body;
+  const route: string = req.url;
+  const user: WithId<Document> = await service.checkIfUserIsRegistered(email, route);
+  const encryptedPassword: string = user.password;
+  const userId: ObjectId = user._id;
+  await service.checkIfPasswordIsValid(password, encryptedPassword);
+  const token: string = await service.generateToken(userId);
+  await service.registerANewActiveSession(userId, token);
+  const data: string = `User ${email} has successfully logged in. \n\n User token: ${token}`;
+  logHandler('Controller', 'User signed in');
+  return res.status(200).send(data);
 }
 
-async function signOutController(
-  req: Request,
-  res: Response
-): Promise<Response> {
+async function signOutController(req: Request, res: Response): Promise<Response> {
   //const { authorization } = req.headers;
   //const token = authorization?.replace('Bearer', '').trim();
 
