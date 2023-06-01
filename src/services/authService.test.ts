8e20cbd90396b39311678796c47ed8cfe991228c
marcos.asdes <marco.engc@poli.ufrj.br>
2023-05-31 23:14:05 -0300

    test: unit tests of encryptPassword and checkIfUserIsRegistered functions were created

--- src/services/authService.test.ts
+++ src/services/authService.test.ts
@@ -9,15 +9,21 @@ describe('checkIfUserIsAlreadyRegistered', () => {
     jest.clearAllMocks();
   });
 
-  /* it('should throw an error if user already exists', async () => {
+  it('should throw an error if user already exists', async () => {
     const email = faker.internet.email();
     const mockUser = { email: email };
     authRepository.findUserInDatabase_ThroughEmail = jest.fn().mockResolvedValue(mockUser);
-    await expect(async () => {
+
+    expect.assertions(2);
+    try {
       await authService.checkIfUserIsAlreadyRegistered(email);
-    }).rejects.toThrow(ErrorLog);
-    expect(authRepository.findUserInDatabase_ThroughEmail).toHaveBeenCalledWith(email);
-  }); */
+    } catch (error) {
+      expect(error).toBeInstanceOf(ErrorLog);
+      if (error instanceof ErrorLog) {
+        expect(error.message).toBe('User already exists');
+      }
+    }
+  });
 
   it('should not throw an error if user does not exist', async () => {
     authRepository.findUserInDatabase_ThroughEmail = jest.fn().mockResolvedValue(null);
@@ -28,5 +34,69 @@ describe('checkIfUserIsAlreadyRegistered', () => {
     expect(authRepository.findUserInDatabase_ThroughEmail).toHaveBeenCalledWith(
       'newuser@example.com'
     );
+  });
+});
+
+describe('encryptPassword', () => {
+  beforeEach(() => {
+    process.env.SALT = '10';
+  });
+
+  afterEach(() => {
+    delete process.env.SALT;
+  });
+
+  it('should encrypt the password', () => {
+    const password = 'mypassword';
+    const encryptedPassword = authService.encryptPassword(password);
+
+    expect(encryptedPassword).toBeDefined();
+    expect(encryptedPassword).not.toBe(password);
+  });
+
+  it('should throw an error if SALT environment variable is not found', () => {
+    delete process.env.SALT;
+
+    expect(() => {
+      authService.encryptPassword('mypassword');
+    }).toThrow(ErrorLog);
+    try {
+      authService.encryptPassword('mypassword');
+    } catch (error) {
+      expect(error).toBeInstanceOf(ErrorLog);
+      if (error instanceof ErrorLog) {
+        expect(error.message).toBe('SALT environment variable not found');
+      }
+    }
+  });
+});
+
+describe('checkIfUserIsRegistered', () => {
+  it('should return the sign-in object if user is registered', async () => {
+    const email = 'test@example.com';
+    const user = { _id: '123', email: 'test@example.com', password: 'hashedPassword' };
+    authRepository.findUserInDatabase_ThroughEmail = jest.fn().mockResolvedValue(user);
+
+    const signInObject = await authService.checkIfUserIsRegistered(email);
+
+    expect(signInObject).toBeDefined();
+    expect(signInObject).toEqual({ userId: '123', encryptedPassword: 'hashedPassword' });
+    expect(authRepository.findUserInDatabase_ThroughEmail).toHaveBeenCalledWith(email);
+  });
+
+  it('should throw an error if user is not found', async () => {
+    const email = 'nonexistent@example.com';
+    authRepository.findUserInDatabase_ThroughEmail = jest.fn().mockResolvedValue(null);
+
+    expect.assertions(3);
+    try {
+      await authService.checkIfUserIsRegistered(email);
+    } catch (error) {
+      expect(error).toBeInstanceOf(ErrorLog);
+      if (error instanceof ErrorLog) {
+        expect(error.message).toBe('User not found');
+      }
+    }
+    expect(authRepository.findUserInDatabase_ThroughEmail).toHaveBeenCalledWith(email);
   });
 });
